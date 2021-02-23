d463f9d17ae262d991fbfab05f88229b6c81af01
mytschez <72097976+mytschez@users.noreply.github.com>
2021-02-23 19:54:25 +0100

    Add files via upload

---
+++ Estadisticas.Designer.vb
@@ -0,0 +1,392 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmEstadisticas
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCargaDatos = New System.Windows.Forms.TextBox()
+        Me.btnCargarDatos = New System.Windows.Forms.Button()
+        Me.btnValidarDatos = New System.Windows.Forms.Button()
+        Me.btnAnalizarDatos = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.lblDe0a10 = New System.Windows.Forms.Label()
+        Me.lblDe11a20 = New System.Windows.Forms.Label()
+        Me.lblDe21a30 = New System.Windows.Forms.Label()
+        Me.lblDe31a40 = New System.Windows.Forms.Label()
+        Me.lblDe41a50 = New System.Windows.Forms.Label()
+        Me.lblDe51a60 = New System.Windows.Forms.Label()
+        Me.lblDe61a70 = New System.Windows.Forms.Label()
+        Me.lblDe71a80 = New System.Windows.Forms.Label()
+        Me.lblDe81a90 = New System.Windows.Forms.Label()
+        Me.lblDe91a100 = New System.Windows.Forms.Label()
+        Me.txtDe0a10 = New System.Windows.Forms.TextBox()
+        Me.txtDe11a20 = New System.Windows.Forms.TextBox()
+        Me.txtDe21a30 = New System.Windows.Forms.TextBox()
+        Me.txtDe31a40 = New System.Windows.Forms.TextBox()
+        Me.txtDe41a50 = New System.Windows.Forms.TextBox()
+        Me.txtDe51a60 = New System.Windows.Forms.TextBox()
+        Me.txtDe61a70 = New System.Windows.Forms.TextBox()
+        Me.txtDe71a80 = New System.Windows.Forms.TextBox()
+        Me.txtDe81a90 = New System.Windows.Forms.TextBox()
+        Me.txtDe91a100 = New System.Windows.Forms.TextBox()
+        Me.OFDArchivo = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'txtCargaDatos
+        '
+        Me.txtCargaDatos.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtCargaDatos.Location = New System.Drawing.Point(12, 12)
+        Me.txtCargaDatos.Multiline = True
+        Me.txtCargaDatos.Name = "txtCargaDatos"
+        Me.txtCargaDatos.ReadOnly = True
+        Me.txtCargaDatos.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtCargaDatos.Size = New System.Drawing.Size(156, 650)
+        Me.txtCargaDatos.TabIndex = 0
+        '
+        'btnCargarDatos
+        '
+        Me.btnCargarDatos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSkyBlue
+        Me.btnCargarDatos.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCargarDatos.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnCargarDatos.Location = New System.Drawing.Point(193, 13)
+        Me.btnCargarDatos.Name = "btnCargarDatos"
+        Me.btnCargarDatos.Size = New System.Drawing.Size(142, 34)
+        Me.btnCargarDatos.TabIndex = 1
+        Me.btnCargarDatos.Text = "Cargar datos"
+        Me.btnCargarDatos.UseVisualStyleBackColor = True
+        '
+        'btnValidarDatos
+        '
+        Me.btnValidarDatos.Enabled = False
+        Me.btnValidarDatos.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnValidarDatos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkSeaGreen
+        Me.btnValidarDatos.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnValidarDatos.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnValidarDatos.Location = New System.Drawing.Point(393, 13)
+        Me.btnValidarDatos.Name = "btnValidarDatos"
+        Me.btnValidarDatos.Size = New System.Drawing.Size(141, 34)
+        Me.btnValidarDatos.TabIndex = 2
+        Me.btnValidarDatos.Text = "Validar datos"
+        Me.btnValidarDatos.UseVisualStyleBackColor = True
+        '
+        'btnAnalizarDatos
+        '
+        Me.btnAnalizarDatos.Enabled = False
+        Me.btnAnalizarDatos.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnAnalizarDatos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkSeaGreen
+        Me.btnAnalizarDatos.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAnalizarDatos.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnAnalizarDatos.Location = New System.Drawing.Point(573, 13)
+        Me.btnAnalizarDatos.Name = "btnAnalizarDatos"
+        Me.btnAnalizarDatos.Size = New System.Drawing.Size(164, 34)
+        Me.btnAnalizarDatos.TabIndex = 3
+        Me.btnAnalizarDatos.Text = "Analizar datos"
+        Me.btnAnalizarDatos.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.IndianRed
+        Me.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSalir.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnSalir.Location = New System.Drawing.Point(943, 13)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(112, 34)
+        Me.btnSalir.TabIndex = 4
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'lblDe0a10
+        '
+        Me.lblDe0a10.AutoSize = True
+        Me.lblDe0a10.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblDe0a10.Location = New System.Drawing.Point(193, 87)
+        Me.lblDe0a10.Name = "lblDe0a10"
+        Me.lblDe0a10.Size = New System.Drawing.Size(179, 21)
+        Me.lblDe0a10.TabIndex = 5
+        Me.lblDe0a10.Text = "Datos entre 0 y 10: "
+        '
+        'lblDe11a20
+        '
+        Me.lblDe11a20.AutoSize = True
+        Me.lblDe11a20.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblDe11a20.Location = New System.Drawing.Point(193, 146)
+        Me.lblDe11a20.Name = "lblDe11a20"
+        Me.lblDe11a20.Size = New System.Drawing.Size(184, 21)
+        Me.lblDe11a20.TabIndex = 6
+        Me.lblDe11a20.Text = "Datos entre 11 y 20:"
+        '
+        'lblDe21a30
+        '
+        Me.lblDe21a30.AutoSize = True
+        Me.lblDe21a30.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblDe21a30.Location = New System.Drawing.Point(193, 205)
+        Me.lblDe21a30.Name = "lblDe21a30"
+        Me.lblDe21a30.Size = New System.Drawing.Size(184, 21)
+        Me.lblDe21a30.TabIndex = 7
+        Me.lblDe21a30.Text = "Datos entre 21 y 30:"
+        '
+        'lblDe31a40
+        '
+        Me.lblDe31a40.AutoSize = True
+        Me.lblDe31a40.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblDe31a40.Location = New System.Drawing.Point(193, 264)
+        Me.lblDe31a40.Name = "lblDe31a40"
+        Me.lblDe31a40.Size = New System.Drawing.Size(184, 21)
+        Me.lblDe31a40.TabIndex = 8
+        Me.lblDe31a40.Text = "Datos entre 31 y 40:"
+        '
+        'lblDe41a50
+        '
+        Me.lblDe41a50.AutoSize = True
+        Me.lblDe41a50.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblDe41a50.Location = New System.Drawing.Point(193, 323)
+        Me.lblDe41a50.Name = "lblDe41a50"
+        Me.lblDe41a50.Size = New System.Drawing.Size(184, 21)
+        Me.lblDe41a50.TabIndex = 9
+        Me.lblDe41a50.Text = "Datos entre 41 y 50:"
+        '
+        'lblDe51a60
+        '
+        Me.lblDe51a60.AutoSize = True
+        Me.lblDe51a60.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblDe51a60.Location = New System.Drawing.Point(193, 382)
+        Me.lblDe51a60.Name = "lblDe51a60"
+        Me.lblDe51a60.Size = New System.Drawing.Size(184, 21)
+        Me.lblDe51a60.TabIndex = 10
+        Me.lblDe51a60.Text = "Datos entre 51 y 60:"
+        '
+        'lblDe61a70
+        '
+        Me.lblDe61a70.AutoSize = True
+        Me.lblDe61a70.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblDe61a70.Location = New System.Drawing.Point(193, 441)
+        Me.lblDe61a70.Name = "lblDe61a70"
+        Me.lblDe61a70.Size = New System.Drawing.Size(184, 21)
+        Me.lblDe61a70.TabIndex = 11
+        Me.lblDe61a70.Text = "Datos entre 61 y 70:"
+        '
+        'lblDe71a80
+        '
+        Me.lblDe71a80.AutoSize = True
+        Me.lblDe71a80.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblDe71a80.Location = New System.Drawing.Point(193, 500)
+        Me.lblDe71a80.Name = "lblDe71a80"
+        Me.lblDe71a80.Size = New System.Drawing.Size(184, 21)
+        Me.lblDe71a80.TabIndex = 12
+        Me.lblDe71a80.Text = "Datos entre 71 y 80:"
+        '
+        'lblDe81a90
+        '
+        Me.lblDe81a90.AutoSize = True
+        Me.lblDe81a90.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblDe81a90.Location = New System.Drawing.Point(193, 559)
+        Me.lblDe81a90.Name = "lblDe81a90"
+        Me.lblDe81a90.Size = New System.Drawing.Size(184, 21)
+        Me.lblDe81a90.TabIndex = 13
+        Me.lblDe81a90.Text = "Datos entre 81 y 90:"
+        '
+        'lblDe91a100
+        '
+        Me.lblDe91a100.AutoSize = True
+        Me.lblDe91a100.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblDe91a100.Location = New System.Drawing.Point(193, 618)
+        Me.lblDe91a100.Name = "lblDe91a100"
+        Me.lblDe91a100.Size = New System.Drawing.Size(194, 21)
+        Me.lblDe91a100.TabIndex = 14
+        Me.lblDe91a100.Text = "Datos entre 91 y 100:"
+        '
+        'txtDe0a10
+        '
+        Me.txtDe0a10.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtDe0a10.Location = New System.Drawing.Point(393, 81)
+        Me.txtDe0a10.Multiline = True
+        Me.txtDe0a10.Name = "txtDe0a10"
+        Me.txtDe0a10.ReadOnly = True
+        Me.txtDe0a10.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDe0a10.Size = New System.Drawing.Size(662, 50)
+        Me.txtDe0a10.TabIndex = 15
+        '
+        'txtDe11a20
+        '
+        Me.txtDe11a20.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtDe11a20.Location = New System.Drawing.Point(393, 140)
+        Me.txtDe11a20.Multiline = True
+        Me.txtDe11a20.Name = "txtDe11a20"
+        Me.txtDe11a20.ReadOnly = True
+        Me.txtDe11a20.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDe11a20.Size = New System.Drawing.Size(662, 50)
+        Me.txtDe11a20.TabIndex = 16
+        '
+        'txtDe21a30
+        '
+        Me.txtDe21a30.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtDe21a30.Location = New System.Drawing.Point(393, 199)
+        Me.txtDe21a30.Multiline = True
+        Me.txtDe21a30.Name = "txtDe21a30"
+        Me.txtDe21a30.ReadOnly = True
+        Me.txtDe21a30.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDe21a30.Size = New System.Drawing.Size(662, 50)
+        Me.txtDe21a30.TabIndex = 17
+        '
+        'txtDe31a40
+        '
+        Me.txtDe31a40.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtDe31a40.Location = New System.Drawing.Point(393, 258)
+        Me.txtDe31a40.Multiline = True
+        Me.txtDe31a40.Name = "txtDe31a40"
+        Me.txtDe31a40.ReadOnly = True
+        Me.txtDe31a40.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDe31a40.Size = New System.Drawing.Size(662, 50)
+        Me.txtDe31a40.TabIndex = 18
+        '
+        'txtDe41a50
+        '
+        Me.txtDe41a50.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtDe41a50.Location = New System.Drawing.Point(393, 317)
+        Me.txtDe41a50.Multiline = True
+        Me.txtDe41a50.Name = "txtDe41a50"
+        Me.txtDe41a50.ReadOnly = True
+        Me.txtDe41a50.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDe41a50.Size = New System.Drawing.Size(662, 50)
+        Me.txtDe41a50.TabIndex = 19
+        '
+        'txtDe51a60
+        '
+        Me.txtDe51a60.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtDe51a60.Location = New System.Drawing.Point(393, 376)
+        Me.txtDe51a60.Multiline = True
+        Me.txtDe51a60.Name = "txtDe51a60"
+        Me.txtDe51a60.ReadOnly = True
+        Me.txtDe51a60.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDe51a60.Size = New System.Drawing.Size(662, 50)
+        Me.txtDe51a60.TabIndex = 20
+        '
+        'txtDe61a70
+        '
+        Me.txtDe61a70.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtDe61a70.Location = New System.Drawing.Point(393, 435)
+        Me.txtDe61a70.Multiline = True
+        Me.txtDe61a70.Name = "txtDe61a70"
+        Me.txtDe61a70.ReadOnly = True
+        Me.txtDe61a70.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDe61a70.Size = New System.Drawing.Size(662, 50)
+        Me.txtDe61a70.TabIndex = 21
+        '
+        'txtDe71a80
+        '
+        Me.txtDe71a80.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtDe71a80.Location = New System.Drawing.Point(393, 494)
+        Me.txtDe71a80.Multiline = True
+        Me.txtDe71a80.Name = "txtDe71a80"
+        Me.txtDe71a80.ReadOnly = True
+        Me.txtDe71a80.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDe71a80.Size = New System.Drawing.Size(662, 50)
+        Me.txtDe71a80.TabIndex = 22
+        '
+        'txtDe81a90
+        '
+        Me.txtDe81a90.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtDe81a90.Location = New System.Drawing.Point(393, 553)
+        Me.txtDe81a90.Multiline = True
+        Me.txtDe81a90.Name = "txtDe81a90"
+        Me.txtDe81a90.ReadOnly = True
+        Me.txtDe81a90.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDe81a90.Size = New System.Drawing.Size(662, 50)
+        Me.txtDe81a90.TabIndex = 23
+        '
+        'txtDe91a100
+        '
+        Me.txtDe91a100.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtDe91a100.Location = New System.Drawing.Point(393, 612)
+        Me.txtDe91a100.Multiline = True
+        Me.txtDe91a100.Name = "txtDe91a100"
+        Me.txtDe91a100.ReadOnly = True
+        Me.txtDe91a100.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDe91a100.Size = New System.Drawing.Size(662, 50)
+        Me.txtDe91a100.TabIndex = 24
+        '
+        'frmEstadisticas
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1070, 687)
+        Me.Controls.Add(Me.txtDe91a100)
+        Me.Controls.Add(Me.txtDe81a90)
+        Me.Controls.Add(Me.txtDe71a80)
+        Me.Controls.Add(Me.txtDe61a70)
+        Me.Controls.Add(Me.txtDe51a60)
+        Me.Controls.Add(Me.txtDe41a50)
+        Me.Controls.Add(Me.txtDe31a40)
+        Me.Controls.Add(Me.txtDe21a30)
+        Me.Controls.Add(Me.txtDe11a20)
+        Me.Controls.Add(Me.txtDe0a10)
+        Me.Controls.Add(Me.lblDe91a100)
+        Me.Controls.Add(Me.lblDe81a90)
+        Me.Controls.Add(Me.lblDe71a80)
+        Me.Controls.Add(Me.lblDe61a70)
+        Me.Controls.Add(Me.lblDe51a60)
+        Me.Controls.Add(Me.lblDe41a50)
+        Me.Controls.Add(Me.lblDe31a40)
+        Me.Controls.Add(Me.lblDe21a30)
+        Me.Controls.Add(Me.lblDe11a20)
+        Me.Controls.Add(Me.lblDe0a10)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnAnalizarDatos)
+        Me.Controls.Add(Me.btnValidarDatos)
+        Me.Controls.Add(Me.btnCargarDatos)
+        Me.Controls.Add(Me.txtCargaDatos)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmEstadisticas"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Estadísticas"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtCargaDatos As TextBox
+    Friend WithEvents btnCargarDatos As Button
+    Friend WithEvents btnValidarDatos As Button
+    Friend WithEvents btnAnalizarDatos As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents lblDe0a10 As Label
+    Friend WithEvents lblDe11a20 As Label
+    Friend WithEvents lblDe21a30 As Label
+    Friend WithEvents lblDe31a40 As Label
+    Friend WithEvents lblDe41a50 As Label
+    Friend WithEvents lblDe51a60 As Label
+    Friend WithEvents lblDe61a70 As Label
+    Friend WithEvents lblDe71a80 As Label
+    Friend WithEvents lblDe81a90 As Label
+    Friend WithEvents lblDe91a100 As Label
+    Friend WithEvents txtDe0a10 As TextBox
+    Friend WithEvents txtDe11a20 As TextBox
+    Friend WithEvents txtDe21a30 As TextBox
+    Friend WithEvents txtDe31a40 As TextBox
+    Friend WithEvents txtDe41a50 As TextBox
+    Friend WithEvents txtDe51a60 As TextBox
+    Friend WithEvents txtDe61a70 As TextBox
+    Friend WithEvents txtDe71a80 As TextBox
+    Friend WithEvents txtDe81a90 As TextBox
+    Friend WithEvents txtDe91a100 As TextBox
+    Friend WithEvents OFDArchivo As OpenFileDialog
+End Class
---
+++ Estadisticas.vb
@@ -0,0 +1,144 @@
+﻿Imports System.IO
+
+Public Class frmEstadisticas
+    'variables
+    Dim i As Integer = 0
+    Dim datos(999) As Integer 'vector de 1000 posiciones para cargar los datos
+    Dim archivo As String 'para guardar la ruta del archivo que queremos abrir
+    Dim sr As StreamReader 'para recorrer el archivo seleccionado
+    Dim longvector As Integer 'para guardar la longitud del archivo cargado
+
+    'Cerrar el formulario al hacer click en el botón salir
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub btnCargarDatos_Click(sender As Object, e As EventArgs) Handles btnCargarDatos.Click
+        CargarDatos()
+    End Sub
+
+    Private Sub btnValidarDatos_Click(sender As Object, e As EventArgs) Handles btnValidarDatos.Click
+        ValidarDatos()
+    End Sub
+
+    Private Sub btnAnalizarDatos_Click(sender As Object, e As EventArgs) Handles btnAnalizarDatos.Click
+        AnalizarDatos()
+    End Sub
+
+#Region "Procedimientos"
+    'procedimiento para cargar los datos
+    Private Sub CargarDatos()
+        Vaciartextbox()
+        'OFDArchivo.ShowDialog() 'para abrir el cuadro de diálogo donde buscaremos el archivo
+        'DialogResult.OK para comprobar que no se cancela el cuadro de dialogo
+        If OFDArchivo.ShowDialog() = DialogResult.OK Then
+            archivo = OFDArchivo.FileName ' para coger el nombre del archivo
+            sr = New StreamReader(archivo)
+            btnValidarDatos.Enabled = True 'Habilitar el botón de validar datos
+            i = 0
+            'carga los datos del fichero hasta 1000
+            While Not sr.EndOfStream And i < 1000
+                'comprobar que los datos que se cargan del fichero tienen el formato correcto
+                Try
+                    datos(i) = Integer.Parse(sr.ReadLine) 'sr.ReadLine() para leer línea por línea
+                    txtCargaDatos.Text = txtCargaDatos.Text & datos(i) & vbCrLf
+                Catch ex As Exception
+                    btnValidarDatos.Enabled = False
+                    MessageBox.Show("El archivo contiene datos con formato incorrecto. Sólo números enteros", "Formato incorrecto")
+                End Try
+                i += 1
+
+            End While
+            longvector = i - 1
+
+            sr.Close() 'cerrar el archivo
+            OFDArchivo.FileName = "" 'vaciar el nombre del archivo
+        Else
+            'si se cancela no hace nada
+            Exit Sub
+
+        End If
+    End Sub
+
+    'Procedimiento que valida los datos
+    Private Sub ValidarDatos()
+        'variables
+        Dim correcto As Boolean = True
+        btnAnalizarDatos.Enabled = True 'Habilitar el botón de analizar datos
+
+        'comprobar que los datos cargados están entre 0 y 100
+        For i = 0 To longvector And correcto
+            If datos(i) < 0 Or datos(i) > 100 Then
+                correcto = False
+            End If
+        Next
+        If correcto Then
+            MessageBox.Show("Los datos se han validado correctamente.")
+        Else
+            MessageBox.Show("Los datos introducidos no son válidos. Los números han de estar entre el 0 y el 100")
+            btnAnalizarDatos.Enabled = False
+        End If
+
+    End Sub
+
+    'procedimiento para analizar los datos
+    Private Sub AnalizarDatos()
+        For i = 0 To longvector
+            If datos(i) >= 0 And datos(i) <= 10 Then
+                txtDe0a10.Text = txtDe0a10.Text & datos(i) & ", "
+            ElseIf datos(i) >= 11 And datos(i) <= 20 Then
+                txtDe11a20.Text = txtDe11a20.Text & datos(i) & ", "
+            ElseIf datos(i) >= 21 And datos(i) <= 30 Then
+                txtDe21a30.Text = txtDe21a30.Text & datos(i) & ", "
+            ElseIf datos(i) >= 31 And datos(i) <= 40 Then
+                txtDe31a40.Text = txtDe31a40.Text & datos(i) & ", "
+            ElseIf datos(i) >= 41 And datos(i) <= 50 Then
+                txtDe41a50.Text = txtDe41a50.Text & datos(i) & ", "
+            ElseIf datos(i) >= 51 And datos(i) <= 60 Then
+                txtDe51a60.Text = txtDe51a60.Text & datos(i) & ", "
+            ElseIf datos(i) >= 61 And datos(i) <= 70 Then
+                txtDe61a70.Text = txtDe61a70.Text & datos(i) & ", "
+            ElseIf datos(i) >= 71 And datos(i) <= 80 Then
+                txtDe71a80.Text = txtDe71a80.Text & datos(i) & ", "
+            ElseIf datos(i) >= 81 And datos(i) <= 90 Then
+                txtDe81a90.Text = txtDe81a90.Text & datos(i) & ", "
+            ElseIf datos(i) >= 91 And datos(i) <= 100 Then
+                txtDe91a100.Text = txtDe91a100.Text & datos(i) & ", "
+            End If
+        Next
+        QuitarUltimacoma()
+        btnValidarDatos.Enabled = False
+        btnAnalizarDatos.Enabled = False
+    End Sub
+    'procedimiento para quitar la última coma
+    Private Sub QuitarUltimacoma()
+        txtDe0a10.Text = txtDe0a10.Text.Remove(txtDe0a10.Text.LastIndexOf(","))
+        txtDe11a20.Text = txtDe11a20.Text.Remove(txtDe11a20.Text.LastIndexOf(","))
+        txtDe21a30.Text = txtDe21a30.Text.Remove(txtDe21a30.Text.LastIndexOf(","))
+        txtDe31a40.Text = txtDe31a40.Text.Remove(txtDe31a40.Text.LastIndexOf(","))
+        txtDe41a50.Text = txtDe41a50.Text.Remove(txtDe41a50.Text.LastIndexOf(","))
+        txtDe51a60.Text = txtDe51a60.Text.Remove(txtDe51a60.Text.LastIndexOf(","))
+        txtDe61a70.Text = txtDe61a70.Text.Remove(txtDe61a70.Text.LastIndexOf(","))
+        txtDe71a80.Text = txtDe71a80.Text.Remove(txtDe71a80.Text.LastIndexOf(","))
+        txtDe81a90.Text = txtDe81a90.Text.Remove(txtDe81a90.Text.LastIndexOf(","))
+        txtDe91a100.Text = txtDe91a100.Text.Remove(txtDe91a100.Text.LastIndexOf(","))
+    End Sub
+    'procedimiento para vaciar los cuadros de texto
+    Private Sub Vaciartextbox()
+        txtCargaDatos.Clear()
+        txtDe0a10.Clear()
+        txtDe11a20.Clear()
+        txtDe21a30.Clear()
+        txtDe31a40.Clear()
+        txtDe41a50.Clear()
+        txtDe51a60.Clear()
+        txtDe61a70.Clear()
+        txtDe71a80.Clear()
+        txtDe81a90.Clear()
+        txtDe91a100.Clear()
+    End Sub
+#End Region
+
+
+
+End Class
